27eb00f9b38edd1d0f85a77821b7a8f63ac369ec
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-03-28 12:58:28 -0600

    Started implementing ArtPanel

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,6 +1,107 @@
 package art.view;
 
-public class ArtPanel
-{
+import java.awt.GridLayout;
+import java.util.Hashtable;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SpringLayout;
+
+import art.controller.Controller;
+
+public class ArtPanel extends JPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private Controller app;
+	
+	private SpringLayout appLayout;
+	private DrawingCanvas canvas;
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton clearButton;
+	private JButton saveButton;
+	private JButton colorButton;
+	
+	private int currentEdgeCount;
+	private int currentScale;
+	
+	public ArtPanel(Controller app) {
+		super();
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MINIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new DrawingCanvas(app);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0,1));
+		
+		triangleButton = new JButton("Add Triangle");
+		rectangleButton = new JButton("Add Rectangle");
+		ellipseButton = new JButton("Add Ellipse");
+		polygonButton = new JButton("Add Polygon");
+		clearButton = new JButton("Clear Image");
+		saveButton = new JButton("Save Image");
+		colorButton = new JButton("Change Color");
+		
+		setupSliders();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupSliders() {
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer,JLabel>();
+		Hashtable<Integer, JLabel> edgeLabels = new Hashtable<Integer,JLabel>();
+		
+		scaleLabels.put(MINIMUM_SCALE, new JLabel("<HTML>Small<BR>Shape</HTML>"));
+		scaleLabels.put((MAXIMUM_SCALE + MINIMUM_SCALE) / 2, new JLabel("<HTML>Medium<BR>Shape</html>"));
+		scaleLabels.put(MAXIMUM_SCALE, new JLabel("<HTML>Large<br>Shape</html>"));
+		
+		edgeLabels.put(MINIMUM_EDGE, new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXIMUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+		
+		scaleSlider.setLabelTable(scaleLabels);
+		scaleSlider.setOrientation(JSlider.VERTICAL);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+		scaleSlider.setPaintTicks(true);
+		scaleSlider.setPaintLabels(true);
+		
+		edgeSlider.setLabelTable(edgeLabels);
+		edgeSlider.setOrientation(JSlider.VERTICAL);
+		edgeSlider.setSnapToTicks(true);
+		edgeSlider.setMajorTickSpacing(3);
+		edgeSlider.setMinorTickSpacing(1);
+		edgeSlider.setPaintTicks(true);
+		edgeSlider.setPaintLabels(true);
+	}
+	
+	private void setupPanel() {
+			
+	}
+	
+	private void setupLayout() {
+		
+	}
+	
+	private void setupListeners() {
+		
+	}
+	
 }
